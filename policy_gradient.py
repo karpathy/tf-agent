245882955d1af95a8d455c7ab83c9cc1b342bb45
karpathy <andrej.karpathy@gmail.com>
2016-10-03 15:49:19 -0700

    a bunch of changes for the better, i think. mostly. kind of.

--- policy_gradient.py
+++ policy_gradient.py
@@ -1,12 +1,12 @@
 """ implements a simple policy gradient (actor critic technically) agent """
 
 import argparse
-import cv2
 import gym
 import time
 from gym.spaces import Discrete
 import numpy as np
 from scipy.signal import lfilter
+from scipy.misc import imsave, imresize
 import tensorflow as tf
 import tensorflow.contrib.slim as slim
 
@@ -15,25 +15,28 @@ parser.add_argument('-e', '--env', default='Breakout-v3', type=str, help='gym en
 parser.add_argument('-b', '--batch_size', default=10000, type=int, help='batch size to use during learning')
 parser.add_argument('-l', '--learning_rate', default=1e-3, type=float, help='used for Adam')
 parser.add_argument('-g', '--discount', default=0.99, type=float, help='reward discount rate to use')
-parser.add_argument('-n', '--hidden_size', default=12, type=int, help='number of hidden units in net')
+parser.add_argument('-n', '--hidden_size', default=20, type=int, help='number of hidden units in net')
 parser.add_argument('-c', '--gradient_clip', default=40.0, type=float, help='clip at this max norm of gradient')
+parser.add_argument('-v', '--value_scale', default=0.5, type=float, help='scale of value function regression in loss')
+parser.add_argument('-t', '--entropy_scale', default=0, type=float, help='scale of entropy penalty in loss')
+parser.add_argument('-m', '--max_steps', default=10000, type=int, help='max number of steps to run for')
 args = parser.parse_args()
+print(args)
 
 # -----------------------------------------------------------------------------
 def process_frame(frame):
     """ Atari specific preprocessing, consistent with DeepMind """
     reshaped_screen = frame.astype(np.float32).mean(2)      # grayscale
-    resized_screen = cv2.resize(reshaped_screen, (84, 110)) # downsample
+    resized_screen = imresize(reshaped_screen, (84, 110)) # downsample
     x = resized_screen[18:102, :]                           # crop top/bottom
-    x = cv2.resize(x, (42, 42))                             # downsample
+    x = imresize(x, (42, 42)).astype(np.float32)                             # downsample
     x *= (1.0 / 255.0)                                      # place in [0,1]
     x = np.reshape(x, [42, 42, 1])                          # introduce channel
     return x
 
 def policy_spec(x):
   net = slim.conv2d(x, args.hidden_size, [5, 5], stride=2, padding='SAME', activation_fn=tf.nn.elu, scope='conv1')
-  net = slim.conv2d(net, args.hidden_size*2, [5, 5], stride=2, padding='SAME', activation_fn=tf.nn.elu, scope='conv2')
-  net = slim.conv2d(net, args.hidden_size*2, [5, 5], stride=2, padding='SAME', activation_fn=tf.nn.elu, scope='conv3')
+  net = slim.conv2d(net, args.hidden_size, [5, 5], stride=2, padding='SAME', activation_fn=tf.nn.elu, scope='conv2')
   net = slim.flatten(net)
   action_logits = slim.fully_connected(net, num_actions, activation_fn=None, scope='fc_act')
   value_function = slim.fully_connected(net, 1, activation_fn=None, scope='fc_value')
@@ -54,6 +57,7 @@ def rollout(n, max_steps_per_episode=4500):
     obf_now = process_frame(ob)
     obf_before = obf_now if prev_obf is None else prev_obf
     obf = np.concatenate((obf_before, obf_now), axis=2)
+    #obf = obf_now - obf_before
     prev_obf = obf_now
 
     # run the policy
@@ -96,12 +100,12 @@ sampled_actions = tf.placeholder(tf.int32, (None,), name='sampled_actions')
 discounted_reward = tf.placeholder(tf.float32, (None,), name='discounted_reward')
 pg_loss = tf.reduce_mean((discounted_reward - value_function) * tf.nn.sparse_softmax_cross_entropy_with_logits(action_logits, sampled_actions))
 # and 2) the baseline (value function) regression piece
-value_loss = 0.5 * tf.reduce_mean(tf.square(discounted_reward - value_function))
+value_loss = args.value_scale * tf.reduce_mean(tf.square(discounted_reward - value_function))
 # and 3) entropy regularization
-#action_log_prob = tf.nn.log_softmax(action_logits)
-#entropy_loss = - 0.01 * tf.reduce_sum(action_log_prob*tf.exp(action_log_prob))
+action_log_prob = tf.nn.log_softmax(action_logits)
+entropy_loss = -args.entropy_scale * tf.reduce_sum(action_log_prob*tf.exp(action_log_prob))
 # add up and minimize
-loss = pg_loss + value_loss #+ entropy_loss
+loss = pg_loss + value_loss + entropy_loss
 # create the optimizer
 optimizer = tf.train.AdamOptimizer(learning_rate=args.learning_rate)
 grads = tf.gradients(loss, tf.trainable_variables())
@@ -113,7 +117,8 @@ train_op = optimizer.apply_gradients(grads_and_vars)
 sess = tf.Session()
 sess.run(tf.initialize_all_variables())
 n = 0
-while True: # loop forever
+mean_rewards = []
+while n <= args.max_steps: # loop forever
   n += 1
 
   # collect a batch of data from rollouts and do forward/backward/update
@@ -123,5 +128,11 @@ while True: # loop forever
   sess.run(train_op, feed_dict={x:observations, sampled_actions:actions, discounted_reward:discounted_reward_np})
   t2 = time.time()
 
-  print 'step %d: collected %d frames in %fs, mean episode reward = %f (%d eps), update in %fs' % \
-        (n, observations.shape[0], t1-t0, np.sum(rewards)/info['num_episodes'], info['num_episodes'], t2-t1)
+  average_reward = np.sum(rewards)/info['num_episodes']
+  mean_rewards.append(average_reward)
+  print('step %d: collected %d frames in %fs, mean episode reward = %f (%d eps), update in %fs' % \
+        (n, observations.shape[0], t1-t0, average_reward, info['num_episodes'], t2-t1))
+
+print(args)
+print('total average reward: %f +/- %f (min %f, max %f)' % \
+      (np.mean(mean_rewards), np.std(mean_rewards), np.min(mean_rewards), np.max(mean_rewards)))
